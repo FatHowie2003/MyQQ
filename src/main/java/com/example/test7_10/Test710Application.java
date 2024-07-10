13df0eedcc72ff7789c52106705a861f27dee73a
肥肥的华仔 <21271203@bjtu.edu.cn>
2024-07-10 21:54:25 +0800

    init

---
+++ src/main/java/com/example/test7_10/Test710Application.java
@@ -0,0 +1,13 @@
+package com.example.test7_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test710Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test710Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test7_10/Test710ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test7_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test710ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
